be2d0663ae7892f21ca18b322e1c4f2cead41294
Tomas Sierra <t.sierras@uniandes.edu.co>
2026-09-17 15:35:18 -0500

    Modelos y repositorios de plan

--- lib/models/plan.dart
+++ lib/models/plan.dart
@@ -1,38 +1,24 @@
-class Participant {
-  final String id;
-  final String initials;
-
-  const Participant({required this.id, required this.initials});
-}
+import 'package:flutter/material.dart';
+import 'package:plansync/models/invitations.dart';
 
 class Plan {
   final String id;
-  final String title;
-  final String date;
-  final int estimatedCostPerPerson;
-  final List<Participant> participants;
-  final int activityCount;
-  final double rating;
-  final String category;
-  final String planType;
+  final String name;
+  final DateTime date;
+  final TimeOfDay meetupTime;
+  final List<String> tags;
+  final String creatorId;
+  final List<String> activityIds;
+  final List<Invitation> invitations;
 
   const Plan({
     required this.id,
-    required this.title,
+    required this.name,
     required this.date,
-    required this.estimatedCostPerPerson,
-    required this.participants,
-    required this.activityCount,
-    this.rating = 0,
-    this.category = '',
-    this.planType = '',
+    required this.meetupTime,
+    required this.creatorId,
+    this.tags = const [],
+    this.activityIds = const [],
+    this.invitations = const [],
   });
-
-  /// Buckets [estimatedCostPerPerson] into a price tier chip label.
-  String get priceTier => switch (estimatedCostPerPerson) {
-    <= 0 => 'Free',
-    <= 30 => r'$',
-    <= 80 => r'$$',
-    _ => r'$$$',
-  };
 }
